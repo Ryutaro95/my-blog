7c1a58ac359170e4e4c2d61a1efff9c8d78db340
Ryutaro <ryutaro9595@gmail.com>
2023-08-19 15:07:11 +0900

    fixed syntax error

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -9,7 +9,6 @@ export default {
   smartypants: {
     dashes: 'oldschool'
   },
-  highlight: ,
   remarkPlugins: [videos, relativeImages, headings],
   rehypePlugins: [
     slugPlugin,
@@ -20,27 +19,6 @@ export default {
       }
     ]
   ]
-}
-
-function highlighter(code, lang, meta){
-  let fence: any
-  let twoslash: any
-  try {
-    fence = parseFence(lex([lang, meta].filter(Boolean).join(' ')))
-  } catch (error) {
-    throw new Error(`Could not parse the codefence for this code sample \n${code}`)
-  }
-  if (fence?.twoslash === true) twoslash = runTwoSlash(code, lang as string)
-  return `{@html \`${escapeSvelte(
-    renderCodeToHTML(
-      code,
-      lang as string,
-      fence ?? {},
-      { themeName: 'material-default' },
-      await createShikiHighlighter({ theme: 'material-default' }),
-      twoslash
-    )
-  )}\` }`
 }
 
 /**
